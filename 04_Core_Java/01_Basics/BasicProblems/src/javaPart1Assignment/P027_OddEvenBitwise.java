03074710e80ec968fc2c8cc5acba47ebb2bbd113
Vedant Gandhewar <vedantgandhewar21@gmail.com>
2026-09-09 22:51:36 +0530

    code added in 01_Basics Assignment

---
+++ 04_Core_Java/01_Basics/BasicProblems/src/javaPart1Assignment/P027_OddEvenBitwise.java
@@ -0,0 +1,21 @@
+package javaPart1Assignment;
+
+import java.util.Scanner;
+
+public class P027_OddEvenBitwise {
+
+	public static void main(String[] args) {
+		System.out.println("Bitwise AND");
+		Scanner sc = new Scanner(System.in);
+		int num1, res;
+		System.out.println("Enter Number to check Odd or Even");
+		num1 = sc.nextInt();
+
+		res = num1 & 1;
+
+		if (res == 1) {
+			System.out.println("Entered Number is Odd");
+		} else
+			System.out.println("Entered Number is Even ");
+	}
+}
