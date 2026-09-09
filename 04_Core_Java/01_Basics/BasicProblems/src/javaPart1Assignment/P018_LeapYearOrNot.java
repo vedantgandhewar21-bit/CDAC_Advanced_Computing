f711a582ffca6ae34c63ca64151d3b4adb83c942
Vedant Gandhewar <vedantgandhewar21@gmail.com>
2026-09-09 23:05:15 +0530

    code added in 01_Basics Assignment

---
+++ 04_Core_Java/01_Basics/BasicProblems/src/javaPart1Assignment/P018_LeapYearOrNot.java
@@ -0,0 +1,19 @@
+package javaPart1Assignment;
+
+import java.util.Scanner;
+
+public class P018_LeapYearOrNot {
+	public static void main(String[] args) {
+		System.out.println("Check if Leap Year Or Not");
+		Scanner sc = new Scanner (System.in);
+		int year;
+		System.out.println("Enter a Year to check");
+		year=  sc.nextInt();
+		if(year%4==0 && year%100!=0||year%400==0) {
+			System.out.println(year+" The Year is a leap year");
+		}
+		else 
+			System.out.println(year +" Not a Leap Year");
+		
+	}
+}
---
+++ 04_Core_Java/01_Basics/BasicProblems/src/javaPart1Assignment/P020_CategorizeIntoDifferentGroup.java
@@ -0,0 +1,25 @@
+package javaPart1Assignment;
+
+import java.util.Scanner;
+
+public class P020_CategorizeIntoDifferentGroup {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Age to Caegorize :");
+		int age = sc.nextInt();
+		
+		if(age<=13) {
+			System.out.println("Child");
+		}
+		else if (age<=20) {
+			System.out.println("Teen");
+		}
+		else if(age <=60) {
+			System.out.println("Adult");
+		}
+		else
+		System.out.println("Senior Citizen");
+		
+			
+	}
+}
